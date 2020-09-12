3f0c807d09d1e1a562f9d5c68490b26360ab9a33
Dhruv Sachdev <dhruvsachdev@Dhruvs-MacBook-Air.local>
2020-09-12 13:33:00 +0530

    first commit

---
+++ lib/SignUpPage.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+
+class SignUpPage extends StatefulWidget {
+  @override
+   _SignUpPageState createState() => _SignUpPageState();
+}
+class _SignUpPageState extends State<SignUpPage> {
+  GlobalKey<FormState> _key = new GlobalKey();
+  bool _autovalidate = false;
+   String name, email, mobile, collegename, password; 
+
+   @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: AppBar(
+        title: Text('SignUp'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0)
+            ),
+            child: Column(
+              children: <Widget>[
+                Padding(padding: EdgeInsets.all(10.0),
+                ),
+                Image(
+                  image: AssetImage("images/logo.png") ,
+                  width: 100.0,
+                  height: 100.0,
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: TextFormField(
+                    validator: (input){
+                      if (input.isEmpty) {
+                        return 'Enter Name';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Name'
+                    ),
+                    onSaved: (input) => name = input,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.email),
+                  title: TextFormField(
+                    validator: (input){
+                      if (input.isEmpty) {
+                        return 'Enter Email';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'email'
+                    ),
+                    onSaved: (input) => email = input,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.lock),
+                  title: TextFormField(
+                    validator: (input){
+                      if (input.isEmpty) {
+                        return 'Enter Password';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'password'
+                    ),
+                    onSaved: (input) => password = input,
+                    obscureText: true,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.phone),
+                  title: TextFormField(
+                    validator: (input){
+                      if (input.isEmpty) {
+                        return 'Enter mobile';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'mobile'
+                    ),
+                    onSaved: (input) => mobile = input,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.school),
+                  title: TextFormField(
+                    validator: (input){
+                      if (input.isEmpty) {
+                        return 'Enter College Name';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'College Name'
+                    ),
+                    onSaved: (input) => collegename = input,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10.0)),
+                ButtonTheme(
+                  height: 40.0,
+                  minWidth: 200.0,
+                  child: RaisedButton(
+                    onPressed: _sendToNextScreen,
+                    color: Colors.redAccent,
+                    child: Text(
+                      'Save',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ), 
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10.0)),
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+  _sendToNextScreen() {
+    if(_key.currentState.validate()) {
+      //saves to global key
+      _key.currentState.save();
+      //send to next screen
+      Navigator.push(context, 
+        MaterialPageRoute(
+          builder: (context) => HomePage(
+            name: name,
+            mobile: mobile,
+            email: email,
+            password: password,
+            collegename: collegename
+          ) 
+        )
+      );
+
+    }
+    else {
+      setState(() {
+        _autovalidate = true;
+      });
+    }
+
+  }
+} 
